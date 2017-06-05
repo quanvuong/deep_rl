c7146864b171ab6059ff81b96e64e27cf1c6fc29
quanvuong <quan.vuong@nyu.edu>
2017-06-05 11:09:33 +0800

    include code to test gpu mem usage on all gpus

--- policy_gradient_batch_baseline.py
+++ policy_gradient_batch_baseline.py
@@ -343,11 +343,11 @@ if __name__ == '__main__':
                           'timestep_reward': 0, 'capture_reward': 1,
                           'end_when_capture': None, 'k': k, 'm': m, 'n': grid_size})
 
-    handle = nvmlDeviceGetHandleByIndex(0)
-    info = nvmlDeviceGetMemoryInfo(handle)
-    print("Total memory:", info.total)
-    print("Free memory:", info.free)
-    print("Used memory:", info.used)
+    handles = [nvmlDeviceGetHandleByIndex(i) for i in range(deviceCount)]
+    infos = [nvmlDeviceGetMemoryInfo(handle) for handle in handles]
+    print("Total memory: {}".format(' '.join(info.total for info in infos)))
+    print("Free memory: {}".format(' '.join(info.free for info in infos)))
+    print("Used memory: {}".format(' '.join(info.used for info in infos)))
 
     for i in range(args.num_rounds):
         policy_net = build_policy_net(policy_net_layers)
@@ -362,7 +362,7 @@ if __name__ == '__main__':
 
         avg_value_error, avg_return = 0.0, 0.0
         for num_episode in range(args.num_episodes):
-            print("Beginning of episode {} Free memory:".format(num_episode), info.free)
+            print("Beginning of episode {} Free memory: {}".format(num_episode, ' '.join(info.free for info in infos)))
             episode = run_episode(policy_net, gamma=args.gamma)
             value_error = train_value_net(value_net, episode, td=args.td_update, gamma=args.gamma)
             avg_value_error = 0.9 * avg_value_error + 0.1 * value_error
