de88668f7a43834c99218ba75ab65a43b7e193ec
quanvuong <quan.vuong@nyu.edu>
2017-06-20 11:54:16 +0800

    set max episode len

--- policy_gradient.py
+++ policy_gradient.py
@@ -316,7 +316,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Runs multi-agent policy gradient.')
     parser.add_argument('--game', choices=['gridworld', 'gridworld_3d', 'hunters'], required=True, help='A game to run')
     parser.add_argument('--cuda', default=False, action='store_true', help='Include to run on CUDA')
-    parser.add_argument('--max_episode_len', default=1000000.0, type=float, help='Terminate episode early at this number of steps')
+    parser.add_argument('--max_episode_len', default=1000.0, type=float, help='Terminate episode early at this number of steps')
     parser.add_argument('--max_len_penalty', default=0, type=float, help='If episode is terminated early, add this to the last reward')
     parser.add_argument('--num_episodes', default=100000, type=int, help='Number of episodes to run in a round of training')
     parser.add_argument('--num_rounds', default=1, type=int, help='How many rounds of training to run')
