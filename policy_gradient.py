a6235bd561106a9388cc33a83a877267de57cc83
Quan Vuong <quan.vuong@nyu.edu>
2017-09-04 16:25:57 +0800

    'n': 5

--- policy_gradient.py
+++ policy_gradient.py
@@ -414,7 +414,7 @@ if __name__ == '__main__':
         value_net_layers = [3*(k+m), 64, 1]
         game.set_options({'rabbit_action': None, 'remove_hunter': True,
                           'timestep_reward': 0, 'capture_reward': 1,
-                          'end_when_capture': None, 'k': k, 'm': m, 'n': 4})
+                          'end_when_capture': None, 'k': k, 'm': m, 'n': 5})
 
     # Set setting for entropy
     try:
